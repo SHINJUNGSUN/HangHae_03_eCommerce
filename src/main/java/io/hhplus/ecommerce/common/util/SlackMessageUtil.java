2e9de5a5194b513392c899fce1c734d5bfab6388
DESKTOP-JI2T3EG\sjshi <sjs818@theloca.co.kr>
2024-11-21 13:01:08 +0900

    Fix: 메시지 전송 로직 보완

--- src/main/java/io/hhplus/ecommerce/common/util/SlackMessageUtil.java
+++ src/main/java/io/hhplus/ecommerce/common/util/SlackMessageUtil.java
@@ -1,5 +1,7 @@
 package io.hhplus.ecommerce.common.util;
 
+import io.hhplus.ecommerce.common.Properties;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
@@ -7,27 +9,24 @@ import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class SlackMessageUtil {
 
-    private final String SLACK_API_URL = "";
-
-    private final String SLACK_TOKEN = "";
-
-    private final String SLACK_CHANNEL_ID = "";
+    private final Properties properties;
 
     public void sendMessage(String message) {
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Bearer " + SLACK_TOKEN);
+        headers.set("Authorization", "Bearer " + properties.getSlackToken());
 
-        String payload = String.format("{\"channel\":\"%s\", \"text\":\"%s\"}", SLACK_CHANNEL_ID, message);
+        String payload = String.format("{\"channel\":\"%s\", \"text\":\"%s\"}", properties.getSlackChannelId(), message);
 
         HttpEntity<String> requestEntity = new HttpEntity<>(payload, headers);
 
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<String> response = restTemplate.exchange(SLACK_API_URL, HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(properties.getSlackApiUrl(), HttpMethod.POST, requestEntity, String.class);
 
         if(response.getStatusCode() == HttpStatus.OK) {
             String responseBody = response.getBody();
--- src/main/java/io/hhplus/ecommerce/payment/application/service/PaymentApplicationService.java
+++ src/main/java/io/hhplus/ecommerce/payment/application/service/PaymentApplicationService.java
@@ -1,6 +1,8 @@
 package io.hhplus.ecommerce.payment.application.service;
 
+import io.hhplus.ecommerce.common.util.SlackMessageUtil;
 import io.hhplus.ecommerce.order.domain.model.Order;
+import io.hhplus.ecommerce.payment.domain.event.PaymentCompleteEvent;
 import io.hhplus.ecommerce.payment.domain.model.Payment;
 import io.hhplus.ecommerce.payment.domain.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,9 +14,17 @@ public class PaymentApplicationService implements PaymentService {
 
     private final PaymentRepository paymentRepository;
 
+    private final SlackMessageUtil slackMessageUtil;
+
     @Override
     public Payment payment(long userSeq, Order order) {
 
         return paymentRepository.save(Payment.ofSuccess(userSeq, order));
+    }
+
+    @Override
+    public void sendDataPlatform(PaymentCompleteEvent event) {
+
+        slackMessageUtil.sendMessage(String.format("사용자(사용자 고유 식별자: %d)가 주문(주문번호: %d)에 대한 결제를 완료하였습니다.", event.getUserSeq(), event.getOrderId()));
     }
 }
